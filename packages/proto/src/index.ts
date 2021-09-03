3c9719f8d25137bf3aa6cb428ac26e97082d015a
Manuel Turetta <manuel.turetta94@gmail.com>
2021-09-03 14:22:41 +0100

    Exported all the messages to interact with the profile module

--- packages/proto/src/index.ts
+++ packages/proto/src/index.ts
@@ -4,10 +4,22 @@ export * from "./desmos/posts/v1beta1/posts";
 export * from "./desmos/posts/v1beta1/reactions";
 export * from "./desmos/posts/v1beta1/report";
 
-
+export * from "./desmos/profiles/v1beta1/models_app_links";
+export * from "./desmos/profiles/v1beta1/models_chain_links";
+export * from "./desmos/profiles/v1beta1/models_dtag_requests";
+export * from "./desmos/profiles/v1beta1/models_packets";
 export * from "./desmos/profiles/v1beta1/models_profile";
+export * from "./desmos/profiles/v1beta1/models_relationships";
+
+export * from "./desmos/profiles/v1beta1/msgs_app_links";
+export * from "./desmos/profiles/v1beta1/msgs_chain_links";
+export * from "./desmos/profiles/v1beta1/msgs_dtag_requests";
 export * from "./desmos/profiles/v1beta1/msgs_profile";
 export * from "./desmos/profiles/v1beta1/msgs_relationships";
-export * from "./desmos/profiles/v1beta1/msgs_dtag_requests";
-export * from "./desmos/profiles/v1beta1/msgs_chain_links";
-export * from "./desmos/profiles/v1beta1/msgs_app_links";
+
+export * from "./desmos/profiles/v1beta1/query_app_links"
+export * from "./desmos/profiles/v1beta1/query_chain_links"
+export * from "./desmos/profiles/v1beta1/query_dtag_requests"
+export * from "./desmos/profiles/v1beta1/query_params"
+export * from "./desmos/profiles/v1beta1/query_profile"
+export * from "./desmos/profiles/v1beta1/query_relationships"
