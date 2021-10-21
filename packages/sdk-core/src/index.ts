a75af003c74be905e5add972f20ce74533ed3d78
Manuel Turetta <manuel.turetta94@gmail.com>
2021-10-21 16:49:27 +0100

    Added a function to convert a coin to his equivalent that has a different exponent.

--- packages/sdk-core/src/index.ts
+++ packages/sdk-core/src/index.ts
@@ -2,6 +2,7 @@ export * from "./desmosclient";
 export * from "./signers";
 export * from "./types";
 export * from "./encodeobjects";
+export * from "./utils";
 
 /* Cosmjs exports */
 export * from "@cosmjs/stargate";
