ae8b12259d1cf652d2f00a23035157e533792d51
Manuel <manuel.turetta94@gmail.com>
2021-11-24 11:48:13 +0100

    Updated the spawn-test-chain script to use desmos v2.3.0-mainnet

--- packages/sdk-core/script/spawn-test-chain.sh
+++ packages/sdk-core/script/spawn-test-chain.sh
@@ -11,13 +11,13 @@ TEST_ACCOUNT_1=desmos1nm6kh6jwqmsezwtnmgdd4w4tzyk9f8gvqu5en0
 TEST_ACCOUNT_2=desmos1cywv6k7d94nyka74q7cr59yauzs690ky2ew6qx
 TEST_ACCOUNT_3=desmos1c7ms9zhtgwmv5jy6ztj2vq0jj67zenw3gdl2gr
 TEST_ACCOUNT_4=desmos1f88f8sdnt4qznzj5drnqtn279uelat402xx5yv
-DESMOS_VERSION="1.0.3"
+DESMOS_VERSION="2.3.0-mainnet"
 DESMOS_URL="https://github.com/desmos-labs/desmos/releases/download/v$DESMOS_VERSION/desmos-$DESMOS_VERSION-linux-amd64"
 DESMOS_BIN="$SCRIPT_DIR/desmos"
 
 if [ -f $DESMOS_BIN ]; then
   BIN_VERSION=$($DESMOS_BIN version)
-  if [ $BIN_VERSION != $DESMOS_VERSION ]; then
+  if [ "$BIN_VERSION" != "$DESMOS_VERSION" ]; then
     echo "Current binary version is != from the expected"
     echo "BIN_VERSION = $BIN_VERSION"
     echo "DESMOS_VERSION = $DESMOS_VERSION"
