ab68cf026efba1edb52ed7b96886178482659a86
Manuel <manuel.turetta94@gmail.com>
2022-07-07 16:00:11 +0200

    refactor: export some cosmjs functions to check if a transaction was succefull or failed

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,5 +4,8 @@ export * from "./queries";
 export * from "./signers";
 export * from "./types";
 export * from "./utils";
-
 export * from "./desmosclient";
+
+// Re-export some cosmjs methods.
+import { assertIsDeliverTxSuccess, assertIsDeliverTxFailure } from "@cosmjs/stargate";
+export { assertIsDeliverTxSuccess, assertIsDeliverTxFailure };
