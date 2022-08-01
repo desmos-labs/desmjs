b773fa6c21a96a62f8fd85a2464a749dbb86eede
Manuel <manuel.turetta94@gmail.com>
2022-08-01 12:53:27 +0200

    build: added lint task to desmjs package

---
+++ packages/core/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: ["airbnb-base", "airbnb-typescript/base", "prettier"],
+  plugins: ["@typescript-eslint", "prettier"],
+  env: {
+    es6: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.eslint.json",
+  },
+  rules: {
+    // Avoiding prettier errors
+    "prettier/prettier": 0,
+    // Permitting console.logs/warns
+    "no-console": 0,
+    // Permitting to define functions wherever you want
+    "no-use-before-define": 0,
+    // Permitting to use the '_' (safely)
+    "no-underscore-dangle": 0,
+    // Strange behavior with default imports
+    "import/no-named-as-default": 0,
+    // Typescript related warnings
+    "@typescript-eslint/no-use-before-define": 0,
+    "@typescript-eslint/no-var-requires": 0,
+    "@typescript-eslint/no-unused-vars": 0,
+  },
+};
