d3fc6d62bfebd42ebeef78bfff237e44f8ea5986
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2022-08-31 10:04:47 +0200

    feat: added missing authz and feegrant EncodeObjects (#48)
    ## Description
    
    This PR adds missing `authz` and `feegrant` EncodeObjects.
    
    
    
    ---
    
    ### Author Checklist
    
    *All items are required. Please add a note to the item if the item is not applicable and
    please add links to any relevant follow up issues.*
    
    I have...
    
    - [x] included the correct [type prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json) in the PR title
    - [x] targeted the correct branch (see [PR Targeting](https://github.com/desmos-labs/desmjs/blob/master/CONTRIBUTING.md#pr-targeting))
    - [ ] provided a link to the relevant issue or specification
    - [x] reviewed "Files changed" and left comments if necessary
    - [x] confirmed all CI checks have passed
    
    ### Reviewers Checklist
    
    *All items are required. Please add a note if the item is not applicable and please add
    your handle next to the items reviewed if you only reviewed selected items.*
    
    I have...
    
    - [x] confirmed the correct [type prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json) in the PR title
    - [x] confirmed all author checklist items have been addressed
    - [ ] confirmed that this PR does not change production code

--- packages/core/src/encodeobjects/cosmos.ts
+++ packages/core/src/encodeobjects/cosmos.ts
@@ -1,8 +1,12 @@
 import { EncodeObject } from "@cosmjs/proto-signing";
-import { MsgGrant } from "cosmjs-types/cosmos/authz/v1beta1/tx";
+import { MsgGrant, MsgRevoke } from "cosmjs-types/cosmos/authz/v1beta1/tx";
+import {
+  MsgGrantAllowance,
+  MsgRevokeAllowance,
+} from "cosmjs-types/cosmos/feegrant/v1beta1/tx";
 
 export interface MsgGrantEncodeObject extends EncodeObject {
-  typeUrl: "/cosmos.authz.v1beta1.MsgGrant";
+  readonly typeUrl: "/cosmos.authz.v1beta1.MsgGrant";
   readonly value: MsgGrant;
 }
 
@@ -12,5 +16,47 @@ export function isMsgGrantEncodeObject(
   return (
     (encodeObject as MsgGrantEncodeObject).typeUrl ===
     "/cosmos.authz.v1beta1.MsgGrant"
+  );
+}
+
+export interface MsgRevokeEncodeObject extends EncodeObject {
+  readonly typeUrl: "/cosmos.authz.v1beta1.MsgRevoke";
+  readonly value: MsgRevoke;
+}
+
+export function isMsgRevoke(
+  encodeObject: EncodeObject
+): encodeObject is MsgRevokeEncodeObject {
+  return (
+    (encodeObject as MsgRevokeEncodeObject).typeUrl ===
+    "/cosmos.authz.v1beta1.MsgRevoke"
+  );
+}
+
+export interface MsgGrantAllowanceEncodeObject extends EncodeObject {
+  readonly typeUrl: "/cosmos.feegrant.v1beta1.MsgGrantAllowance";
+  readonly value: MsgGrantAllowance;
+}
+
+export function isMsgGrantAllowance(
+  encodeObject: EncodeObject
+): encodeObject is MsgGrantAllowanceEncodeObject {
+  return (
+    (encodeObject as MsgGrantAllowanceEncodeObject).typeUrl ===
+    "/cosmos.feegrant.v1beta1.MsgGrantAllowance"
+  );
+}
+
+export interface MsgRevokeAllowanceEncodeObject extends EncodeObject {
+  readonly typeUrl: "/cosmos.feegrant.v1beta1.MsgRevokeAllowance";
+  readonly value: MsgRevokeAllowance;
+}
+
+export function isMsgRevokeAllowance(
+  encodeObject: EncodeObject
+): encodeObject is MsgRevokeAllowanceEncodeObject {
+  return (
+    (encodeObject as MsgRevokeAllowanceEncodeObject).typeUrl ===
+    "/cosmos.feegrant.v1beta1.MsgRevokeAllowance"
   );
 }
