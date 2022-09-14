42458f554500634198e6f5b86c8d81dfe94850f7
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2022-09-14 13:40:03 +0200

    feat: add timestamp utils methods

---
+++ packages/core/src/utils/timestamp.unit.spec.ts
@@ -0,0 +1,25 @@
+import { Timestamp } from "@desmoslabs/desmjs-types/build/google/protobuf/timestamp";
+import Long from "long";
+import { timestampFromDate, timestampToDate } from "./timestamp";
+
+describe("Utils test", () => {
+  describe("Conversions", () => {
+    it("Convert date to timestamp and back", () => {
+      const date = new Date();
+      const timestamp = timestampFromDate(date);
+      const converted = timestampToDate(timestamp);
+      expect(converted.getTime()).toBe(date.getTime());
+    });
+
+    it("Convert timestamp to date and back", () => {
+      const timestamp: Timestamp = {
+        seconds: Long.fromNumber(1577876400),
+        nanos: 0,
+      };
+      const date = timestampToDate(timestamp);
+      const converted = timestampFromDate(date);
+      expect(converted.seconds.toString()).toBe(timestamp.seconds.toString());
+      expect(converted.nanos).toBe(timestamp.nanos);
+    });
+  });
+});
