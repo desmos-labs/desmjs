c79db11de48c7e366257fdd1f3fd57d1a2af428a
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2022-09-14 11:13:07 +0200

    fix: fix Amino Long conversions

--- packages/core/src/aminomessages/posts/messages.ts
+++ packages/core/src/aminomessages/posts/messages.ts
@@ -1,12 +1,11 @@
 import { AminoMsg } from "@cosmjs/amino";
-import Long from "long";
 import { ReplySetting } from "@desmoslabs/desmjs-types/desmos/posts/v2/models";
 import { AminoAttachment, AminoEntities, AminoPostReference } from "./types";
 
 export interface AminoMsgCreatePost extends AminoMsg {
   readonly type: "desmos/MsgCreatePost";
   readonly value: {
-    subspace_id: Long;
+    subspace_id: string;
     section_id: number;
     external_id: string;
     text: string;
@@ -14,7 +13,7 @@ export interface AminoMsgCreatePost extends AminoMsg {
     tags: string[];
     attachments: AminoAttachment[];
     author: string;
-    conversation_id: Long;
+    conversation_id: string;
     reply_settings: ReplySetting;
     referenced_posts: AminoPostReference[];
   };
@@ -23,8 +22,8 @@ export interface AminoMsgCreatePost extends AminoMsg {
 export interface AminoMsgEditPost extends AminoMsg {
   readonly type: "desmos/MsgEditPost";
   readonly value: {
-    subspace_id: Long;
-    post_id: Long;
+    subspace_id: string;
+    post_id: string;
     text: string;
     entities?: AminoEntities;
     tags: string[];
@@ -35,8 +34,8 @@ export interface AminoMsgEditPost extends AminoMsg {
 export interface AminoMsgDeletePost extends AminoMsg {
   readonly type: "desmos/MsgDeletePost";
   readonly value: {
-    subspace_id: Long;
-    post_id: Long;
+    subspace_id: string;
+    post_id: string;
     signer: string;
   };
 }
@@ -44,8 +43,8 @@ export interface AminoMsgDeletePost extends AminoMsg {
 export interface AminoMsgAddPostAttachment extends AminoMsg {
   readonly type: "desmos/MsgAddPostAttachment";
   readonly value: {
-    subspace_id: Long;
-    post_id: Long;
+    subspace_id: string;
+    post_id: string;
     content: AminoAttachment;
     editor: string;
   };
@@ -54,8 +53,8 @@ export interface AminoMsgAddPostAttachment extends AminoMsg {
 export interface AminoMsgRemovePostAttachment extends AminoMsg {
   readonly type: "desmos/MsgRemovePostAttachment";
   readonly value: {
-    subspace_id: Long;
-    post_id: Long;
+    subspace_id: string;
+    post_id: string;
     attachment_id: number;
     editor: string;
   };
@@ -64,8 +63,8 @@ export interface AminoMsgRemovePostAttachment extends AminoMsg {
 export interface AminoMsgAnswerPoll extends AminoMsg {
   readonly type: "desmos/MsgAnswerPoll";
   readonly value: {
-    subspace_id: Long;
-    post_id: Long;
+    subspace_id: string;
+    post_id: string;
     poll_id: number;
     answers_indexes: number[];
     signer: string;
