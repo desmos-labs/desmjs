4cb798ef3d96e91b6c0002607f741966980d1a5d
Manuel Turetta <manuel.turetta94@gmail.com>
2023-01-13 15:33:07 -0500

    feat: add functions to decode the wallet connect requests

--- packages/walletconnect-v2/src/types.ts
+++ packages/walletconnect-v2/src/types.ts
@@ -1,9 +1,12 @@
-import {StdSignature, StdSignDoc} from "@cosmjs/amino";
+import { StdFee, StdSignature, StdSignDoc } from "@cosmjs/amino";
+import { SignClientTypes } from "@walletconnect/types";
+import { SignDoc } from "cosmjs-types/cosmos/tx/v1beta1/tx";
+import { EncodeObject } from "@cosmjs/proto-signing";
 
-export const CosmosRPCMethods = {
-  GetAccounts: "cosmos_getAccounts",
-  SignAmino: "cosmos_signAmino",
-  SignDirect: "cosmos_signDirect"
+export enum CosmosRPCMethods {
+  GetAccounts = "cosmos_getAccounts",
+  SignAmino = "cosmos_signAmino",
+  SignDirect = "cosmos_signDirect",
 }
 
 /**
@@ -13,55 +16,119 @@ export interface SerializedAccountData {
   /**
    * Account bech32 address.
    */
-  address: string
+  address: string;
   /**
    * Algorithm used to generate the pubkey.
    */
-  algo: string
+  algo: string;
   /**
    * Hex encoded public key.
    */
-  pubkey: string
+  pubkey: string;
 }
 
 /**
  * Interface that represents the response of the cosmos_getAccounts RPC method.
  */
 export interface GetAccountsRpcResponse {
-  accounts: SerializedAccountData[],
+  accounts: SerializedAccountData[];
 }
 
 /**
  * Interface that represents the params of the cosmos_signDirect RPC method.
  */
 export interface SignDirectRpcRequestParams {
-  signerAddress: string,
+  signerAddress: string;
   // Hex encoded body bytes
-  bodyBytes: string,
+  bodyBytes: string;
   // Hex encoded auth info bytes
-  authInfoBytes: string,
-  chainId: string,
-  accountNumber: string
+  authInfoBytes: string;
+  chainId: string;
+  accountNumber: string;
+}
+
+export interface SignDirectDecodedRpcRequestParams {
+  signerAddress: string;
+  signDoc: SignDoc;
 }
 
 /**
  * Interface that represents the response of the cosmos_signDirect RPC method.
  */
 export interface SignDirectRpcResponseParams {
-  signature: StdSignature
+  signature: StdSignature;
 }
 
 /**
  * Interface that represents the params of the cosmos_signAmino RPC method.
  */
 export interface SignAminoRpcRequestParams {
-  signerAddress: string
-  signDoc: StdSignDoc
+  signerAddress: string;
+  signDoc: StdSignDoc;
+}
+
+export interface SignAminoDecodedRpcRequestParams {
+  signerAddress: string;
+  signDoc: StdSignDoc;
 }
 
 /**
  * Interface that represents the response of the cosmos_signAmino RPC method.
  */
 export interface SignAminoRpcResponseParams {
-  signature: StdSignature
+  signature: StdSignature;
 }
+
+/**
+ * Common fields that each WalletConnect request have.
+ */
+interface BaseWalletConnectRequest {
+  readonly request: SignClientTypes.EventArguments["session_request"];
+  /**
+   * Request id to be used in the response method of [SignClient].
+   */
+  readonly id: number;
+  readonly topic: string;
+}
+
+/**
+ * Interface that represents a cosmos_getAccounts RPC request.
+ */
+export interface WalletConnectGetAccountsRequest
+  extends BaseWalletConnectRequest {
+  readonly method: CosmosRPCMethods.GetAccounts;
+}
+
+/**
+ * Interface that represents a cosmos_signAmino RPC request.
+ */
+export interface WalletConnectSignAminoRequest
+  extends BaseWalletConnectRequest {
+  readonly method: CosmosRPCMethods.SignAmino;
+  readonly signerAddress: string;
+  readonly signDoc: StdSignDoc;
+  readonly fee: StdFee;
+  readonly msgs: EncodeObject[];
+  readonly memo?: string;
+}
+
+/**
+ * Interface that represents a cosmos_signDirect RPC request.
+ */
+export interface WalletConnectSignDirectRequest
+  extends BaseWalletConnectRequest {
+  readonly method: CosmosRPCMethods.SignDirect;
+  readonly signerAddress: string;
+  readonly signDoc: SignDoc;
+  readonly fee: StdFee;
+  readonly msgs: EncodeObject[];
+  readonly memo?: string;
+}
+
+/**
+ * Type that represents the current supported WalletConnect RPC requests.
+ */
+export type WalletConnectRequest =
+  | WalletConnectGetAccountsRequest
+  | WalletConnectSignAminoRequest
+  | WalletConnectSignDirectRequest;
