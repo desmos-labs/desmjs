1351b0749041287591bc6d3b069fc7818a66cf14
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2023-01-16 10:35:00 -0500

    fix: fixed build error

--- packages/core/src/aminomessages/desmjs/messages.ts
+++ packages/core/src/aminomessages/desmjs/messages.ts
@@ -2,7 +2,7 @@ import { AminoMsg } from "@cosmjs/amino";
 import { MsgAuthenticateAminoType } from "../../const";
 
 export interface AminoMsgAuthenticate extends AminoMsg {
-  readonly type: MsgAuthenticateAminoType;
+  readonly type: typeof MsgAuthenticateAminoType;
   readonly value: {
     user: string;
     nonce: Uint8Array;
