dc85a2dfdc722dd2581ce75331c08cf587bdad2f
Manuel <manuel.turetta94@gmail.com>
2023-01-16 09:46:15 -0500

    feat: add functions to decode WalletConnect requests (#219)
    ## Description
    
    
    
    This PR adds some utilty functions to decode the WalletConnect session requests.
    
    ---
    
    ### Author Checklist
    
    *All items are required. Please add a note to the item if the item is not applicable and
    please add links to any relevant follow up issues.*
    
    I have...
    
    - [x] included the correct [type prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json) in the PR title
    - [x] targeted the correct branch (see [PR Targeting](https://github.com/desmos-labs/desmjs/blob/master/CONTRIBUTING.md#pr-targeting))
    - [ ] provided a link to the relevant issue or specification
    - [x] reviewed "Files changed" and left comments if necessary
    - [x] confirmed all CI checks have passed
    
    ### Reviewers Checklist
    
    *All items are required. Please add a note if the item is not applicable and please add
    your handle next to the items reviewed if you only reviewed selected items.*
    
    I have...
    
    - [x] confirmed the correct [type prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json) in the PR title
    - [x] confirmed all author checklist items have been addressed
    - [ ] confirmed that this PR does not change production code

--- packages/walletconnect-v2/src/decode.ts
+++ packages/walletconnect-v2/src/decode.ts
@@ -1,23 +1,33 @@
-import {
-  SerializedAccountData,
-  SignAminoRpcRequestParams,
-  SignDirectRpcRequestParams,
-} from "./types";
-import {fromHex} from "@cosmjs/encoding";
-import {AccountData, Algo, StdSignDoc} from "@cosmjs/amino";
-import {SignDoc} from "cosmjs-types/cosmos/tx/v1beta1/tx";
+import { fromHex } from "@cosmjs/encoding";
+import { AccountData, Algo, StdFee } from "@cosmjs/amino";
 import Long from "long";
+import { SignClientTypes } from "@walletconnect/types";
+import { createDesmosTypes, desmosRegistryTypes } from "@desmoslabs/desmjs";
+import { EncodeObject, Registry } from "@cosmjs/proto-signing";
+import { AuthInfo, TxBody } from "cosmjs-types/cosmos/tx/v1beta1/tx";
+import { AminoTypes } from "@cosmjs/stargate";
+import {
+  CosmosRPCMethods,
+  SignAminoDecodedRpcRequestParams,
+  SignAminoRpcRequestParams,
+  SignDirectDecodedRpcRequestParams,
+  SignDirectRpcRequestParams,
+  WalletConnectRequest,
+  WalletConnectSignAminoRequest,
+  WalletConnectSignDirectRequest,
+} from "./types";
 
 /**
  * Class that represents decoding result.
  */
 export class DecodeResult<T> {
-  private readonly _value?: T
-  private readonly _error?: string
+  private readonly _value?: T;
+
+  private readonly _error?: string;
 
   private constructor(value?: T, error?: string) {
-    this._value = value
-    this._error = error
+    this._value = value;
+    this._error = error;
   }
 
   /**
@@ -25,7 +35,7 @@ export class DecodeResult<T> {
    * @param value - The decoded value.
    */
   public static ok<T>(value: T): DecodeResult<T> {
-    return new DecodeResult<T>(value)
+    return new DecodeResult<T>(value);
   }
 
   /**
@@ -33,7 +43,7 @@ export class DecodeResult<T> {
    * @param message - The error occurred during the decoding procedure.
    */
   public static error(message: string): DecodeResult<any> {
-    return new DecodeResult(undefined, message)
+    return new DecodeResult(undefined, message);
   }
 
   /**
@@ -41,14 +51,14 @@ export class DecodeResult<T> {
    * false otherwise.
    */
   public isError(): boolean {
-    return this._error !== undefined
+    return this._error !== undefined;
   }
 
   /**
    * Gets the error occurred during the decoding procedure.
    */
   get error(): string {
-    return this._error!
+    return this._error!;
   }
 
   /**
@@ -63,7 +73,21 @@ export class DecodeResult<T> {
    * Gets the decode value.
    */
   get value(): T {
-    return this._value!
+    return this._value!;
+  }
+
+  /**
+   * Maps the current value using the provided mapFunc.
+   * If this DecodeResult is error the mapFunc will return a new DecodeResult
+   * with the previous error.
+   * @param mapFunc - Function that converts the [DecodeResult] value to
+   * the new one
+   */
+  public map<M>(mapFunc: (value: T) => M): DecodeResult<M> {
+    if (this.isError()) {
+      return DecodeResult.error(this.error);
+    }
+    return DecodeResult.ok(mapFunc(this.value));
   }
 }
 
@@ -71,41 +95,51 @@ export class DecodeResult<T> {
  * Decodes the response received from the cosmos_getAccounts RPC method.
  * @param response - The response to decode.
  */
-export function decodeGetAccountsRpcResponse(response: any | undefined): DecodeResult<AccountData[]> {
+export function decodeGetAccountsRpcResponse(
+  response: any | undefined
+): DecodeResult<AccountData[]> {
   if (response === undefined) {
     return DecodeResult.error("the response is undefined");
   }
 
-  const accounts: Partial<SerializedAccountData>[] | undefined = response["accounts"];
+  const { accounts } = response;
 
   if (accounts === undefined) {
-    return DecodeResult.error("missing accounts field in response")
+    return DecodeResult.error("missing accounts field in response");
   }
 
   const deserializedAccounts: AccountData[] = [];
-  const validAccountAlgo = ["secp256k1", "ed25519", "sr25519"]
+  const validAccountAlgo = ["secp256k1", "ed25519", "sr25519"];
 
-  for (let i = 0; i < accounts.length; i++) {
+  for (let i = 0; i < accounts.length; i += 1) {
     const serializedAccount = accounts[i];
     if (serializedAccount.address === undefined) {
-      return DecodeResult.error(`missing address field in account at index ${i}`);
+      return DecodeResult.error(
+        `missing address field in account at index ${i}`
+      );
     }
     if (serializedAccount.algo === undefined) {
-      return DecodeResult.error(`missing address field in account at index ${i}`);
+      return DecodeResult.error(
+        `missing address field in account at index ${i}`
+      );
     }
     if (serializedAccount.pubkey === undefined) {
-      return DecodeResult.error(`missing address field in account at index ${i}`);
+      return DecodeResult.error(
+        `missing address field in account at index ${i}`
+      );
     }
 
     if (validAccountAlgo.indexOf(serializedAccount.algo) === -1) {
-      return DecodeResult.error(`invalid account algo ${serializedAccount.algo} at index ${i}`);
+      return DecodeResult.error(
+        `invalid account algo ${serializedAccount.algo} at index ${i}`
+      );
     }
 
     deserializedAccounts.push({
       address: serializedAccount.address,
       algo: serializedAccount.algo as Algo,
       pubkey: fromHex(serializedAccount.pubkey),
-    })
+    });
   }
 
   return DecodeResult.ok(deserializedAccounts);
@@ -115,26 +149,27 @@ export function decodeGetAccountsRpcResponse(response: any | undefined): DecodeR
  * Decodes the params received from the cosmos_signDirect RPC method.
  * @param params - The params to decode.
  */
-export function decodeDirectSignRpcRequestParams(params: any[]): DecodeResult<{
-  signerAddress: string,
-  signDoc: SignDoc
-}> {
+export function decodeDirectSignRpcRequestParams(
+  params: any[]
+): DecodeResult<SignDirectDecodedRpcRequestParams> {
   if (params.length === 0) {
     return DecodeResult.error("empty params");
   }
   const requestParams = params[0] as Partial<SignDirectRpcRequestParams>;
-  const keys = ["signerAddress", "authInfoBytes", "bodyBytes", "chainId", "accountNumber"];
+  const keys: (keyof SignDirectRpcRequestParams)[] = [
+    "signerAddress",
+    "authInfoBytes",
+    "bodyBytes",
+    "chainId",
+    "accountNumber",
+  ];
 
-  const missingFields = [];
-  for (let key of keys) {
-    // @ts-ignore
-    if (requestParams[key] === undefined) {
-      missingFields.push(key)
-    }
-  }
+  const missingFields = keys.filter((k) => requestParams[k] === undefined);
 
   if (missingFields.length > 0) {
-    return DecodeResult.error(`missing fields in request: ${missingFields.join(",")}`);
+    return DecodeResult.error(
+      `missing fields in request: ${missingFields.join(",")}`
+    );
   }
 
   return DecodeResult.ok({
@@ -143,8 +178,8 @@ export function decodeDirectSignRpcRequestParams(params: any[]): DecodeResult<{
       bodyBytes: fromHex(requestParams.bodyBytes!),
       authInfoBytes: fromHex(requestParams.authInfoBytes!),
       chainId: requestParams.chainId!,
-      accountNumber: Long.fromString(requestParams.accountNumber!)
-    }
+      accountNumber: Long.fromString(requestParams.accountNumber!),
+    },
   });
 }
 
@@ -152,31 +187,194 @@ export function decodeDirectSignRpcRequestParams(params: any[]): DecodeResult<{
  * Decodes the params received from the cosmos_signAmino RPC method.
  * @param params - The params to decode.
  */
-export function decodeAminoSignRpcRequestParams(params: any[]): DecodeResult<{
-  signerAddress: string,
-  signDoc: StdSignDoc
-}> {
+export function decodeAminoSignRpcRequestParams(
+  params: any[]
+): DecodeResult<SignAminoDecodedRpcRequestParams> {
   if (params.length === 0) {
     return DecodeResult.error("empty params");
   }
   const requestParams = params[0] as Partial<SignAminoRpcRequestParams>;
-  const keys = ["signerAddress", "signDoc"];
+  const keys: (keyof SignAminoDecodedRpcRequestParams)[] = [
+    "signerAddress",
+    "signDoc",
+  ];
 
-  const missingFields = [];
-  for (let key of keys) {
-    // @ts-ignore
-    if (requestParams[key] === undefined) {
-      missingFields.push(key)
-    }
-  }
+  const missingFields = keys.filter((k) => requestParams[k] === undefined);
 
   if (missingFields.length > 0) {
-    return DecodeResult.error(`missing fields in request: ${missingFields.join(",")}`);
+    return DecodeResult.error(
+      `missing fields in request: ${missingFields.join(",")}`
+    );
   }
 
   return DecodeResult.ok({
     signerAddress: requestParams.signerAddress!,
-    signDoc: requestParams.signDoc!
+    signDoc: requestParams.signDoc!,
   });
 }
 
+/**
+ * Function to decode a WalletConnect "cosmos_signDirect" request.
+ * @param request - The request to convert.
+ */
+export function decodeDirectSignRequest(
+  request: SignClientTypes.EventArguments["session_request"]
+): DecodeResult<WalletConnectSignDirectRequest> {
+  const { method, params } = request.params.request;
+  if (method !== CosmosRPCMethods.SignDirect) {
+    return DecodeResult.error(
+      `invalid method, can decode only ${CosmosRPCMethods.SignDirect}`
+    );
+  }
+
+  const paramsDecodeResult = decodeDirectSignRpcRequestParams(params);
+  if (paramsDecodeResult.isError()) {
+    return DecodeResult.error(paramsDecodeResult.error);
+  }
+
+  const registry = new Registry(desmosRegistryTypes);
+  const { signDoc, signerAddress } = paramsDecodeResult.value;
+  let txBody: TxBody;
+  let authInfo: AuthInfo;
+
+  try {
+    txBody = TxBody.decode(signDoc.bodyBytes);
+  } catch (e) {
+    return DecodeResult.error("error while decoding txBody");
+  }
+
+  try {
+    authInfo = AuthInfo.decode(signDoc.authInfoBytes);
+  } catch (e) {
+    return DecodeResult.error("error while decoding authInfo");
+  }
+
+  const typesWithDecodeResult: [string, EncodeObject | undefined][] =
+    txBody.messages.map((msg) => {
+      try {
+        return [
+          msg.typeUrl,
+          {
+            typeUrl: msg.typeUrl,
+            value: registry.decode(msg),
+          } as EncodeObject,
+        ];
+      } catch (e) {
+        return [msg.typeUrl, undefined];
+      }
+    });
+
+  const failedTypes = typesWithDecodeResult
+    .filter(([, decoded]) => decoded === undefined)
+    .map(([typeUrl]) => typeUrl);
+
+  if (failedTypes.length > 0) {
+    return DecodeResult.error(`failed to decode ${failedTypes.join(",")}`);
+  }
+
+  // Safe to cast to EncodeObject since here we have just defined decoded objects.
+  const msgs = typesWithDecodeResult.map(
+    ([, decoded]) => <EncodeObject>decoded
+  );
+
+  const fee: StdFee = {
+    gas: authInfo.fee?.gasLimit?.toString() ?? "",
+    amount: authInfo.fee?.amount ?? [],
+  };
+
+  return DecodeResult.ok({
+    method: CosmosRPCMethods.SignDirect,
+    id: request.id,
+    topic: request.topic,
+    request,
+    signerAddress,
+    signDoc,
+    msgs,
+    fee,
+    memo: txBody.memo,
+  });
+}
+
+/**
+ * Function to decode a WalletConnect "cosmos_signAmino" request.
+ * @param request - The request to convert.
+ */
+export function decodeAminoSignRequest(
+  request: SignClientTypes.EventArguments["session_request"]
+): DecodeResult<WalletConnectSignAminoRequest> {
+  const { method, params } = request.params.request;
+  if (method !== CosmosRPCMethods.SignAmino) {
+    return DecodeResult.error(
+      `invalid method, can decode only ${CosmosRPCMethods.SignAmino}`
+    );
+  }
+
+  const paramsDecodeResult = decodeAminoSignRpcRequestParams(params);
+  if (paramsDecodeResult.isError()) {
+    return DecodeResult.error(paramsDecodeResult.error);
+  }
+
+  const { signDoc, signerAddress } = paramsDecodeResult.value;
+  const aminoConverter = new AminoTypes(createDesmosTypes("desmos"));
+  const typesWithDecodeResult: [string, EncodeObject | undefined][] =
+    signDoc.msgs.map((msg) => {
+      try {
+        const converted = aminoConverter.fromAmino(msg);
+        return [msg.type, converted];
+      } catch (e) {
+        return [msg.type, undefined];
+      }
+    });
+
+  const failedTypes = typesWithDecodeResult
+    .filter(([, decoded]) => decoded === undefined)
+    .map(([type]) => type);
+
+  if (failedTypes.length > 0) {
+    return DecodeResult.error(`failed to decode ${failedTypes.join(",")}`);
+  }
+
+  // Safe to cast to EncodeObject since here we have just defined decoded objects.
+  const msgs = typesWithDecodeResult.map(
+    ([, encodeObject]) => <EncodeObject>encodeObject
+  );
+
+  return DecodeResult.ok({
+    method: CosmosRPCMethods.SignAmino,
+    id: request.id,
+    topic: request.topic,
+    request,
+    signDoc,
+    signerAddress,
+    msgs,
+    fee: signDoc.fee,
+    memo: signDoc.memo,
+  });
+}
+
+/**
+ * Function to decode a WalletConnect request received through the
+ * "session_request" event listener.
+ * @param request - The request to decode.
+ */
+export function decodeSessionRequest(
+  request: SignClientTypes.EventArguments["session_request"]
+): DecodeResult<WalletConnectRequest> {
+  const { method } = request.params.request;
+
+  switch (method) {
+    case CosmosRPCMethods.GetAccounts:
+      return DecodeResult.ok({
+        method: CosmosRPCMethods.GetAccounts,
+        request,
+        id: request.id,
+        topic: request.topic,
+      });
+    case CosmosRPCMethods.SignAmino:
+      return decodeAminoSignRequest(request);
+    case CosmosRPCMethods.SignDirect:
+      return decodeDirectSignRequest(request);
+    default:
+      return DecodeResult.error(`unsupported method ${method}`);
+  }
+}
---
+++ packages/walletconnect-v2/src/decode.unit.spec.ts
@@ -0,0 +1,175 @@
+import { SignClientTypes } from "@walletconnect/types";
+import { AuthInfo, TxBody } from "cosmjs-types/cosmos/tx/v1beta1/tx";
+import {
+  AminoMsgSaveProfile,
+  MsgSaveProfileEncodeObject,
+  MsgSaveProfileTypeUrl,
+} from "@desmoslabs/desmjs";
+import { MsgSaveProfile } from "@desmoslabs/desmjs-types/desmos/profiles/v3/msgs_profile";
+import { AminoMsg, Coin, StdFee } from "@cosmjs/amino";
+import Long from "long";
+import {
+  CosmosRPCMethods,
+  SignAminoRpcRequestParams,
+  SignDirectRpcRequestParams,
+  WalletConnectSignAminoRequest,
+  WalletConnectSignDirectRequest,
+} from "./types";
+import { decodeSessionRequest } from "./decode";
+import {
+  encodeAminoSignRpcRequestParams,
+  encodeDirectSignRpcRequestParams,
+} from "./encode";
+
+type RequestEvent = SignClientTypes.EventArguments["session_request"];
+
+const TEST_SIGNER = "desmos...";
+const TEST_MEMO = "test-memo";
+const TEST_ACCOUNT_NUMBER = Long.fromNumber(5);
+const TEST_CHAIN_ID = "test-chain";
+const TEST_FEE_AMOUNT: Coin[] = [{ denom: "udsm", amount: "1000" }];
+const TEST_GAS = "2000";
+const TEST_STD_FEE: StdFee = {
+  amount: TEST_FEE_AMOUNT,
+  gas: TEST_GAS,
+};
+
+function mockEventRequest(method: string, params?: any): RequestEvent {
+  return {
+    id: 0,
+    topic: "test-topic",
+    params: {
+      chainId: "",
+      request: {
+        method,
+        params: [params],
+      },
+    },
+  };
+}
+
+function mockDirectRequestArguments(
+  messages: { typeUrl: string; value: Uint8Array }[]
+): SignDirectRpcRequestParams {
+  return encodeDirectSignRpcRequestParams(TEST_SIGNER, {
+    bodyBytes: TxBody.encode(
+      TxBody.fromPartial({
+        memo: TEST_MEMO,
+        messages,
+      })
+    ).finish(),
+    authInfoBytes: AuthInfo.encode(
+      AuthInfo.fromPartial({
+        fee: {
+          amount: TEST_FEE_AMOUNT,
+          gasLimit: TEST_GAS,
+        },
+      })
+    ).finish(),
+    chainId: TEST_CHAIN_ID,
+    accountNumber: TEST_ACCOUNT_NUMBER,
+  });
+}
+
+function mockAminoRequestArguments(
+  msgs: AminoMsg[]
+): SignAminoRpcRequestParams {
+  return encodeAminoSignRpcRequestParams(TEST_SIGNER, {
+    fee: TEST_STD_FEE,
+    msgs,
+    memo: TEST_MEMO,
+    sequence: "1",
+    chain_id: TEST_CHAIN_ID,
+    account_number: TEST_ACCOUNT_NUMBER.toString(),
+  });
+}
+
+describe("WalletConnect decode", () => {
+  it("decode cosmos_getAccounts successfully", () => {
+    const requestEvent = mockEventRequest(CosmosRPCMethods.GetAccounts);
+    const decodeResult = decodeSessionRequest(requestEvent);
+
+    expect(decodeResult.isOk()).toBe(true);
+    const decodedValue = decodeResult.value;
+    expect(decodedValue.method).toBe(CosmosRPCMethods.GetAccounts);
+    expect(decodedValue.id).toBe(requestEvent.id);
+    expect(decodedValue.topic).toBe(requestEvent.topic);
+  });
+
+  it("decode cosmos_signDirect successfully", () => {
+    const msgSaveProfile = MsgSaveProfile.fromPartial({
+      bio: "test-bio",
+      dtag: "test-dtag",
+      creator: "desmos",
+      nickname: "nickname",
+      coverPicture: "cover picture",
+      profilePicture: "profile picture",
+    });
+
+    const requestEvent = mockEventRequest(
+      CosmosRPCMethods.SignDirect,
+      mockDirectRequestArguments([
+        {
+          typeUrl: MsgSaveProfileTypeUrl,
+          value: MsgSaveProfile.encode(msgSaveProfile).finish(),
+        },
+      ])
+    );
+    const decodeResult = decodeSessionRequest(requestEvent);
+
+    expect(decodeResult.isOk()).toBe(true);
+    const decodedValue = <WalletConnectSignDirectRequest>decodeResult.value;
+    expect(decodedValue.method).toBe(CosmosRPCMethods.SignDirect);
+    expect(decodedValue.id).toBe(requestEvent.id);
+    expect(decodedValue.topic).toBe(requestEvent.topic);
+    expect(decodedValue.memo).toBe(TEST_MEMO);
+    expect(decodedValue.fee).toEqual(TEST_STD_FEE);
+    expect(decodedValue.msgs).toEqual([
+      {
+        typeUrl: MsgSaveProfileTypeUrl,
+        value: msgSaveProfile,
+      } as MsgSaveProfileEncodeObject,
+    ]);
+  });
+
+  it("decode cosmos_signAmino successfully", () => {
+    const msgSaveProfile: AminoMsgSaveProfile = {
+      type: "desmos/MsgSaveProfile",
+      value: {
+        bio: "test-bio",
+        dtag: "test-dtag",
+        creator: "desmos",
+        nickname: "nickname",
+        cover_picture: "cover picture",
+        profile_picture: "profile picture",
+      },
+    };
+
+    const requestEvent = mockEventRequest(
+      CosmosRPCMethods.SignAmino,
+      mockAminoRequestArguments([msgSaveProfile])
+    );
+    const decodeResult = decodeSessionRequest(requestEvent);
+
+    expect(decodeResult.isOk()).toBe(true);
+    const decodedValue = <WalletConnectSignAminoRequest>decodeResult.value;
+    expect(decodedValue.method).toBe(CosmosRPCMethods.SignAmino);
+    expect(decodedValue.id).toBe(requestEvent.id);
+    expect(decodedValue.topic).toBe(requestEvent.topic);
+    expect(decodedValue.memo).toBe(TEST_MEMO);
+    expect(decodedValue.fee).toEqual(TEST_STD_FEE);
+    expect(decodedValue.msgs).toEqual([
+      {
+        typeUrl: MsgSaveProfileTypeUrl,
+        value: {
+          bio: msgSaveProfile.value.bio,
+          dtag: msgSaveProfile.value.dtag,
+          creator: msgSaveProfile.value.creator,
+          nickname: msgSaveProfile.value.nickname,
+          coverPicture: msgSaveProfile.value.cover_picture,
+          profilePicture: msgSaveProfile.value.profile_picture,
+        },
+      } as MsgSaveProfileEncodeObject,
+    ]);
+  });
+});
