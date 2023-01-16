25a31635e2f06ed49d97f1bf3d4b842c32789738
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2023-01-16 17:09:51 -0500

    feat: added more Cosmos messages type const

---
+++ packages/core/src/const/cosmos/distribution/index.ts
@@ -0,0 +1,18 @@
+export const MsgWithdrawDelegatorRewardTypeUrl =
+  "/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward";
+export const MsgWithdrawDelegatorRewardAminoType =
+  "cosmos-sdk/MsgWithdrawDelegatorReward";
+
+export const MsgSetWithdrawAddressTypeUrl =
+  "/cosmos.distribution.v1beta1.MsgSetWithdrawAddress";
+export const MsgSetWithdrawAddressAminoType =
+  "cosmos-sdk/MsgSetWithdrawAddress";
+
+export const MsgWithdrawValidatorCommissionTypeUrl =
+  "/cosmos.distribution.v1beta1.MsgWithdrawValidatorCommission";
+export const MsgWithdrawValidatorCommissionAminoType =
+  "cosmos-sdk/MsgWithdrawValidatorCommission";
+
+export const MsgFundCommunityPoolTypeUrl =
+  "/cosmos.distribution.v1beta1.MsgFundCommunityPool";
+export const MsgFundCommunityPoolAminoType = "cosmos-sdk/MsgFundCommunityPool";
---
+++ packages/core/src/const/cosmos/gov/index.ts
@@ -0,0 +1,8 @@
+export const MsgSubmitProposalTypeUrl = "/cosmos.gov.v1beta.MsgSubmitProposal";
+export const MsgSubmitProposalAminoType = "cosmos-sdk/MsgSubmitProposal";
+
+export const MsgDepositTypeUrl = "/cosmos.gov.v1beta.MsgDeposit";
+export const MsgDepositAminoType = "cosmos-sdk/MsgDeposit";
+
+export const MsgVoteTypeUrl = "/cosmos.gov.v1beta.MsgVote";
+export const MsgVoteAminoType = "cosmos-sdk/MsgVote";
