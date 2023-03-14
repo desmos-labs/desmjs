301e9afcbfb6b6b34910d496f5bf615fe1dda037
Manuel Turetta <manuel.turetta94@gmail.com>
2023-03-14 17:08:06 -0500

    fix: exposed the index.ts and helper.ts of @desmoslabs/desmjs-types

--- packages/types/script/prepare.sh
+++ packages/types/script/prepare.sh
@@ -5,8 +5,19 @@ command -v shellcheck >/dev/null && shellcheck "$0"
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 DIRS="confio cosmos desmjs desmos ethermint gogoproto google ibc tendermint"
+FILES=(
+  helpers.d.ts
+  helpers.js
+  index.d.ts
+  index.js
+)
 
 for dir in $DIRS; do
   rm -rf "$SCRIPT_DIR/../$dir"
   cp -R "$SCRIPT_DIR/../build/$dir" "$SCRIPT_DIR/../."
+done
+
+for f in ${FILES[*]}; do
+  rm -rf "$SCRIPT_DIR/../$f"
+  cp "$SCRIPT_DIR/../build/$f" "$SCRIPT_DIR/../."
 done
