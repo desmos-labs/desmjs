b1ca52cec4197643a62190e33e64070b4b61454a
Manuel Turetta <manuel.turetta94@gmail.com>
2023-06-28 11:54:34 -0500

    build(deps): bump @web3auth/react-native-sdk to 4.0.0

--- packages/web3auth-mobile/src/privatekeyprovider.ts
+++ packages/web3auth-mobile/src/privatekeyprovider.ts
@@ -4,27 +4,20 @@ import {
   PrivateKeyProviderStatus,
   PrivateKeyType,
 } from "@desmoslabs/desmjs";
-import Web3Auth, {
-  SdkLoginParams,
-  SdkLogoutParams,
-} from "@web3auth/react-native-sdk";
+import Web3Auth, { SdkLoginParams } from "@web3auth/react-native-sdk";
 import { fromHex } from "@cosmjs/encoding";
 import { Platform } from "react-native";
 
 export interface Web3AuthKeyProviderParams {
   /**
    * Parameters used to perform the login.
-   * For more details see https://web3auth.io/docs/sdk/react-native/usage#login
+   * For more details see https://web3auth.io/docs/sdk/pnp/react-native/usage#login
    * Note: the curve field will be overridden to secp256k1.
    */
   loginParams: Omit<SdkLoginParams, "curve">;
   /**
-   * Params used to perform the logout.
-   */
-  logoutParams: SdkLogoutParams;
-  /**
    * If true performs the logout operation on iOS even if is not recommended.
-   * See here for more details https://web3auth.io/docs/sdk/react-native/usage#logout
+   * See here for more details https://web3auth.io/docs/sdk/pnp/react-native/usage#logout
    */
   triggerLogoutOnIos?: boolean;
 }
@@ -33,8 +26,6 @@ export class Web3AuthKeyProvider extends PrivateKeyProvider {
   private readonly web3auth: Web3Auth;
 
   private readonly loginParams: SdkLoginParams;
-
-  private readonly logoutParams: SdkLogoutParams;
 
   private readonly triggerLogoutOnIos: boolean;
 
@@ -48,22 +39,31 @@ export class Web3AuthKeyProvider extends PrivateKeyProvider {
       // Force the private key to secp256k1 since is the only one supported from the private key signer.
       curve: "secp256k1",
     };
-    this.logoutParams = params.logoutParams;
     this.triggerLogoutOnIos = params.triggerLogoutOnIos ?? false;
   }
 
   async connect(): Promise<void> {
     this.updateStatus(PrivateKeyProviderStatus.Connecting);
-    const response = await this.web3auth.login(this.loginParams);
+    await this.web3auth.login(this.loginParams);
 
-    if (response.privKey === undefined) {
+    let hexPrivKey: string;
+    try {
+      hexPrivKey = this.web3auth.privKey;
+    } catch (e) {
+      // Catch the exception thrown if coreKitKey is not available
+      // to update the key provider state.
+      this.updateStatus(PrivateKeyProviderStatus.NotConnected);
+      throw e;
+    }
+
+    if (hexPrivKey === undefined || hexPrivKey === "") {
       this.updateStatus(PrivateKeyProviderStatus.NotConnected);
       throw new Error("can't connect privateKey is undefined");
     }
 
     this.privateKey = {
       type: PrivateKeyType.Secp256k1,
-      key: fromHex(response.privKey),
+      key: fromHex(hexPrivKey),
     };
 
     this.updateStatus(PrivateKeyProviderStatus.Connected);
@@ -74,7 +74,7 @@ export class Web3AuthKeyProvider extends PrivateKeyProvider {
 
     if (Platform.OS !== "ios" || this.triggerLogoutOnIos) {
       try {
-        await this.web3auth.logout(this.logoutParams);
+        await this.web3auth.logout();
       } catch (e) {
         this.updateStatus(PrivateKeyProviderStatus.NotConnected);
         throw e;
