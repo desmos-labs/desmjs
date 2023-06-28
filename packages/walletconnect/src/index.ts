46fd920b652567990e5da90042a50dade7ebfc70
Manuel Turetta <manuel.turetta94@gmail.com>
2023-06-28 16:02:56 -0500

    chore: drop support to WalletConnect v1

--- packages/walletconnect/src/index.ts
+++
@@ -1,7 +0,0 @@
-import WalletConnect from "@walletconnect/client";
-import QRCodeModal from "@walletconnect/qrcode-modal";
-
-export * from "./signer";
-
-// Export the WalletConnect types
-export { WalletConnect, QRCodeModal };
