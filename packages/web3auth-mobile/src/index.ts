4a0928a7679bd6cec79de6a9582d78f412d358e6
Manuel Turetta <manuel.turetta94@gmail.com>
2023-07-13 16:41:59 -0500

    feat: export Web3Auth of  @web3auth/react-native-sdk

--- packages/web3auth-mobile/src/index.ts
+++ packages/web3auth-mobile/src/index.ts
@@ -1,1 +1,5 @@
+// Exports Web3Auth imports so that who use this library don't need to
+// install @web3auth/react-native-sdk
+export * from "@web3auth/react-native-sdk";
+export { default as Web3Auth } from "@web3auth/react-native-sdk";
 export * from "./privatekeyprovider";
