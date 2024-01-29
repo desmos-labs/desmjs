f14dfe1f0c4bc07badc192b4240b53d249bd2b44
Manuel <manuel.turetta94@gmail.com>
2024-01-29 15:06:56 +0100

    feat: allow to customize the modal displayed

--- packages/walletconnect-modal/src/index.tsx
+++ packages/walletconnect-modal/src/index.tsx
@@ -1,8 +1,30 @@
 import React from "react";
 import ReactDom from "react-dom";
-import Modal from "./modal";
+import Modal, { ModalUIOptions } from "./modal";
 
-export default class WalletConnectModal {
+export interface DPMWalletConnectModalOptions {
+  /**
+   * Option to customize the modal.
+   */
+  readonly uiOptions?: ModalUIOptions;
+  /**
+   * Option to customize the URL that is generated to trigger the
+   * mobile app open when the user visit the site trough a mobile device.
+   */
+  readonly appUrlGenerator?: (uri: string, isMobile: boolean) => string;
+}
+
+export default class DPMWalletConnectModal {
+  private readonly options: DPMWalletConnectModalOptions | undefined;
+
+  /**
+   * Constructor for the DPMWalletConnectModal.
+   * @param options - Options to customize the DPMWalletConnectModal.
+   */
+  constructor(options?: DPMWalletConnectModalOptions) {
+    this.options = options;
+  }
+
   /**
    *  Function to open the modal and display the QR code
    *  that the user need to scan in order to start the
@@ -22,6 +44,8 @@ export default class WalletConnectModal {
           this.close();
           onClose();
         }}
+        uiOptions={this.options?.uiOptions}
+        appUrlGenerator={this.options?.appUrlGenerator}
       />,
       wrapper,
     );
